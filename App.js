713757e5235d6c53bb80123e1da4008257266051
rohsi <rohitsinha4826@gmail.com>
2018-06-12 12:23:55 +0530

    react-js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+      return(
+      <div className='App'>
+          <h1> hi I'm React App!!</h1>
+      </div>
+  );
+    React.createElement('div',null,React.createElement('h1','null','Do You Work At Company?'));
+  }
+}
+
+export default App;
