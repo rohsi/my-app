c3bd79f5a51e904bfab0cbaebf169e26ad030b80
rohsi <rohitsinha4826@gmail.com>
2018-06-13 12:00:53 +0530

    react-js

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person.js'
+import person from "./Person";
 
 class App extends Component {
-  render() {
+    state = {
+        persons: [
+            {name:'Rohit', age:'28'},
+                {name:'Mohit', age:'25'},
+            {name:'Pankaj' ,age:'57'}
+        ]
+
+}
+switchNameHandler = () => {
+        console.log('Was Clicked!');
+}
+            render()
+     {
       return(
-      <div className='App'>
-          <h1> hi I'm React App!!</h1>
-      </div>
-  );
-    React.createElement('div',null,React.createElement('h1','null','Do You Work At Company?'));
+          <div className="App">
+              <h1>Do U Work At Company</h1>
+              <button onClick={this.switchNameHandler}>Switch Name</button>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+              <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies:Playing games on mobile phone</Person>
+              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+          </div>
+      );
+    return React.createElement('div',null,React.createElement('h1',{className:'App'},'Do You Work At Company?'));
   }
 }
 
---
+++ Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const person = (props) =>{
+    return <div><h3>I'm {props.name} and currently I am {props.age} years old</h3>
+        <p>{props.children}</p>
+    </div>
+}
+export default person;
