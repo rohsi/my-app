1ad67a4d78698f7c0b6ca34a23f5f7e3f968649c
rohsi <rohitsinha4826@gmail.com>
2018-06-14 17:11:07 +0530

    react-js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person.js'
@@ -7,28 +7,39 @@ import person from "./Person";
 class App extends Component {
     state = {
         persons: [
-            {name:'Rohit', age:'28'},
-                {name:'Mohit', age:'25'},
-            {name:'Pankaj' ,age:'57'}
-        ]
+            {name: 'Rohit', age: '28'},
+            {name: 'Mohit', age: '25'},
+            {name: 'Pankaj', age: '57'}
+        ],
+        otherstate: "some other value"
 
-}
-switchNameHandler = () => {
-        console.log('Was Clicked!');
-}
-            render()
-     {
-      return(
-          <div className="App">
-              <h1>Do U Work At Company</h1>
-              <button onClick={this.switchNameHandler()}>Switch Name</button>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-              <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies:Playing games on mobile phone</Person>
-              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-          </div>
-      );
-     React.createElement('div',null,React.createElement('h1',{className:'App'},'Do You Work At Company?'));
-  }
+    }
+    switchNameHandler = () => {
+        console.log("herllosdf")
+        //console.log('Was Clicked!');
+        //Dont do this this.state.persons[0].name='maxmillian';
+        this.setState({
+            persons: [
+                {name: 'Rohit Sinha', age: '29'},
+                {name: 'Mohit', age: '25'},
+                {name: 'Pankaj', age: '57'}
+            ]
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Do U Work At Company</h1>
+                <button onClick={this.switchNameHandler()}>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies:Playing games on
+                    mobile phone</Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+            </div>
+        );
+        // React.createElement('div',null,React.createElement('h1',{className:'App'},'Do You Work At Company?'));
+    }
 }
 
 export default App;
