b8cccf5ee7d4cf58da1330603f45216787bdd0e2
rohsi <rohitsinha4826@gmail.com>
2018-06-18 15:08:24 +0530

    react-js

---
+++ Shorter code after removing person.js
@@ -0,0 +1,82 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Person from './Person.js'
+
+
+class App extends Component {
+    state = {
+        persons: [
+            {name: 'Rohit', age: '28'},
+            {name: 'Mohit', age: '25'},
+            {name: 'Pankaj', age: '57'}
+        ],
+        otherstate: "some other value",
+        showPersons:false
+
+    }
+    switchNameHandler = (newName) => {
+
+        //console.log('Was Clicked!');
+        //Dont do this this.state.persons[0].name='maxmillian';
+        this.setState({
+            persons: [
+                {name: newName, age: '29'},
+                {name: 'Mohit', age: '25'},
+                {name: 'Pankaj', age: '57'}
+            ]
+        })
+    }
+    nameChangedHandler = (event) => {
+        this.setState({
+            persons: [
+                {name: 'Rohit', age: '29'},
+                {name: event.target.value, age: '25'},
+                {name: 'Pankaj', age: '57'}
+            ]
+        })
+    }
+    togglePersonHandler =()=>{
+        const doesShow=this.state.showPersons;
+        this.setState({showPersons:!doesShow});
+    }
+
+    render() {
+        const style=
+            {
+                backgroundColor: 'white',
+                font: 'inherit',
+                border: '1px solid blue',
+                padding: '8px',
+                cursor:'pointer'
+            };
+        let persons=null;
+        if (this.state.showPersons) {
+            persons =(
+                <div>
+                    {this.state.persons.map(person=>{
+                        return <Person name={person.name} age={person.age}/>
+                    })}
+
+                </div>
+
+            );
+
+        }
+        return (
+            <div className="App">
+                <h1>Do U Work At Company</h1>
+                <button
+                    style={style}
+                    onClick={this.togglePersonHandler}>Toggle Persons
+                </button>
+
+                {persons}
+
+            </div>
+        );
+        // React.createElement('div',null,React.createElement('h1',{className:'App'},'Do You Work At Company?'));
+    }
+}
+
+export default App;
