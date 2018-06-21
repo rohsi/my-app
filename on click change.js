582733c7c7e53b0779b9e46be80894b452c5e842
rohsi <rohitsinha4826@gmail.com>
2018-06-21 10:55:45 +0530

    react-js

---
+++ on click change.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './person.css';
+const person = (props) =>{
+    return <div className="Person" >
+        <p onClick={props.click}>I'm {props.name} and currently I am {props.age} years old</p>
+        <p >{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name}/>
+    </div>
+}
+export default person;
